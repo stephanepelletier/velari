b7489191e3be8ae9416cc235d5378dee932cedef
Stéphane Pelletier <stephane@Mac.lan>
2026-04-09 11:56:23 -1000

    fix entrypoint et routing

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+module.exports = app;
