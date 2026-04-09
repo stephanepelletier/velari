5547fa7b63541de50df81033d65147902520bf05
Stéphane Pelletier <stephane@Mac.lan>
2026-04-09 12:22:53 -1000

    static + node routing

--- server.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  const fs = require('fs');
-  const path = require('path');
-  const file = path.join(__dirname, 'public', 'index.html');
-  res.setHeader('Content-Type', 'text/html');
-  res.end(fs.readFileSync(file));
-};
